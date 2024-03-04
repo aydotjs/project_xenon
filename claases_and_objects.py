fecfdffb70f5ca9024d58e86fc658aa87bbd2c42
aydotjs <ayobami66@gmail.com>
2024-03-04 22:04:59 +0100

    added parameter to the constructor function

--- claases_and_objects.py
+++ claases_and_objects.py
@@ -1,3 +1,4 @@
+"""
 class Person:
     def __init__(self, name):
         # self allows to attach parameter to the class
@@ -7,3 +8,22 @@ class Person:
 p = Person('Ayobami')
 print(p.name)
 print(p)
+
+"""
+class Person:
+    def __init__(self, firstname, lastname, age, country, city):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.age = age
+        self.country = country
+        self.city = city
+    def info(self):
+        return f"{self.firstname} {self.lastname} is {self.age} years old and lives in {self.city}, {self.country}"
+
+ciroma = Person('Ciroma', 'Adekunle', 20, 'Nigeria', 'Ilorin')
+print(ciroma.info())
+print(ciroma.firstname)
+print(ciroma.lastname)
+print(ciroma.age)
+print(ciroma.country)
+print(ciroma.city)
